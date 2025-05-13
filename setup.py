386aba4727f9d952f3ecffa4978ab2a54553fe01
Tal Wrii <talwrii@gmail.com>
2025-05-13 15:41:07 +0200

    bump release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import distutils.core
 
 setuptools.setup(
     name='kde-wallpaper-cli',
-    version="1.1.0",
+    version="1.2.0",
     author='@readwithai',
     long_description_content_type='text/markdown',
     author_email='talwrii@gmail.com',
