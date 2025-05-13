f8fc9682a708d22f30a151cc21c645c969e36646
Tal Wrii <talwrii@gmail.com>
2025-05-13 15:16:23 +0200

    add url

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import distutils.core
 
 setuptools.setup(
     name='kde-wallpaper-cli',
-    version="1.0.1",
+    version="1.1.0",
     author='@readwithai',
     long_description_content_type='text/markdown',
     author_email='talwrii@gmail.com',
     description='Set the wallpaper for KDE',
     license='MIT',
     keywords='KDE,wallpaper',
-    url='',
+    url='https://github.com/talwrii/kde-wallpaper-cli',
     packages=["kde_wallpaper_cli"],
     long_description=open('README.md').read(),
     entry_points={
